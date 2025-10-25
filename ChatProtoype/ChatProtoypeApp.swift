ba7bc0889e85823159b688f2656bd325539c2f3d
Fernando Caudillo Tafoya <fcaudillotafoya@icloud.com>
2025-10-24 19:51:30 -0500

    Initial Commit

---
+++ ChatProtoype/ChatProtoypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatProtoypeApp.swift
+//  ChatProtoype
+//
+//  Created by Fernando Caudillo Tafoya on 10/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatProtoypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
